4ca0e0fcd916432273f22aaceb3d86b9621aa633
balogh.norbert <balogh.norbert92@gmail.com>
2025-04-15 14:02:30 +0200

    project init with db

---
+++ src/main/java/hu/nje/vrs/vehiclerepairsystem/VehicleRepairSystemApplication.java
@@ -0,0 +1,13 @@
+package hu.nje.vrs.vehiclerepairsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleRepairSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleRepairSystemApplication.class, args);
+	}
+
+}
